82b1e60d2a3bdced0bb6a123f649a25d43714d93
stéphanie <sbelveaux@gmail.com>
2024-09-24 14:36:36 +0200

    tickethack carts

--- routes/carts.js
+++ routes/carts.js
@@ -2,16 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 require('../models/connection')
-//const Cart = require('../models/users');
-
+const Cart = require('../models/carts');
+const Order = require('../models/orders')
 
 
 // créer une route qui affiche les "trips" ajoutés à la collection "carts"
+router.get('/carts', (req, res) => {
+    Cart.find()
+      .then(data => res.json({ carts: data }))
+  });
 
+// créer une route qui envoie les "trips" de "carts" vers "ORDERS" au clic sur "purchase" et vide le cart
+
+  router.post('/orders', (req, res) => {
+    const trips = req.body.departure
+    const total = 
+  
+    const newOrder = new Order({
+      trips: departure,
+      total: arrival,
+  
+    })
+    Order.find()
+      .then(data => {
+        console.log(data)
+        res.json({ orders: data })
+      })
+    newOrder.save();
+  
+  
+  })
 //**Clic Purchase** = route post vers "orders", panier vidé, renvoie vers la page =>Bookings
 
 // créer une route qui envoie les "trips" de "carts" vers "ORDERS" au clic sur "purchase" et vide le cart
 
 //créer une fonction TOTAL montant des trips
-
-//**Clic Purchase** = route post vers "orders", panier vidé, renvoie vers la page =>Bookings
