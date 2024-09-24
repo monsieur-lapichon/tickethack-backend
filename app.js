28c93d27f4ad4526156995225ab4ec316ea53db8
David Poixblanc <lapichon@gmail.com>
2024-09-24 14:34:02 +0200

    route booking

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bookingsRouter = require('./routes/bookings');
 
 var app = express();
 
@@ -16,5 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bookings', bookingsRouter);
 
 module.exports = app;
---
+++ routes/bookings.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+const fetch = require('node-fetch');
+const Order = require('../models/orders');
+
+
+router.get('/', (req, res) => {
+	Order.find().then(data => {
+		res.json({ weather: data });
+	});
+});
+
+router.get("/:cityName", (req, res) => {
+  Order.findOne({
+    cityName: { $regex: new RegExp(req.params.cityName, "i") },
+  }).then(data => {
+    if (data) {
+      res.json({ result: true, weather: data });
+    } else {
+      res.json({ result: false, error: "City not found" });
+    }
+  });
+});
+
+
+module.exports = router;
