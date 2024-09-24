ad0c9a309f65fdc689d6d6cd4ec959c554f3c8ba
stéphanie <sbelveaux@gmail.com>
2024-09-24 14:56:46 +0200

    trips/type

--- routes/carts.js
+++ routes/carts.js
@@ -6,6 +6,7 @@ const Cart = require('../models/carts');
 const Order = require('../models/orders')
 
 
+
 // créer une route qui affiche les "trips" ajoutés à la collection "carts"
 router.get('/carts', (req, res) => {
     Cart.find()
@@ -13,9 +14,9 @@ router.get('/carts', (req, res) => {
   });
 
 // créer une route qui envoie les "trips" de "carts" vers "ORDERS" au clic sur "purchase" et vide le cart
-
-  router.post('/orders', (req, res) => {
-    const trips = req.body.departure
+// vider le panier => delete
+ /* router.post('/orders', (req, res) => {
+    const trips = 
     const total = 
   
     const newOrder = new Order({
@@ -31,7 +32,10 @@ router.get('/carts', (req, res) => {
     newOrder.save();
   
   
-  })
+  })  */
+
+
+
 //**Clic Purchase** = route post vers "orders", panier vidé, renvoie vers la page =>Bookings
 
 // créer une route qui envoie les "trips" de "carts" vers "ORDERS" au clic sur "purchase" et vide le cart
