05428668dc1c3e1ca69f1b04b0c93088cd2f2f45
David Poixblanc <lapichon@gmail.com>
2024-09-25 10:02:21 +0200

    maj booking route

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,22 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-//const fetch = require('node-fetch');
-const Order = require('../models/orders');
-const Trip = require('../models/trips');
-
+const Cart = require('../models/carts');
 
 router.get('/', (req, res) => {
-	Order.findOne()
-  .populate('trips')
-  .then(data => {
-
-
+	Cart.find({ isPaid: true })
+	.then(data => {
 		res.json({ bookings: data });
 	});
 });
-
-
-
 
 module.exports = router;
